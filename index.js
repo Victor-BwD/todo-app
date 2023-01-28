2a5cab5314ee479e367d8a651c240dbaa9f55799
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-01-28 02:25:38 -0300

    Post method

--- index.js
+++ index.js
@@ -10,5 +10,11 @@ app.get('/',(req, res) => {
     res.render('todo.ejs');
 });
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/',(req, res) => {
+    console.log(req.body);
+});
+
 
 app.listen(3000, () => console.log("Server up and running!"));
